e7790117d83711ceeef3fa7a39022ed53d746602
Artur Meshcheryakov <arthur.188@yandex.ru>
2020-07-10 23:03:26 +0300

    lesson26

--- script/script.js
+++ script/script.js
@@ -399,6 +399,132 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   };
 
+  //send-ajax-form
+  const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...';
+    const successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+    const form = document.querySelectorAll('form');
+    const statusMessage = document.createElement('div');
+    statusMessage.classList.add('status__massage');
+    statusMessage.style.cssText = `
+      font-size: 2rem;
+    `;
+
+    const formSubmit = (form) => {
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        form.append(statusMessage);
+
+        let elementsForm = [...form.elements].filter((item) => {
+          return item.tagName.toLowerCase() !== 'button' && item.type !== 'button';
+        });
+
+        let bool = true;
+        let button = event.submitter;
+
+        elementsForm.forEach((item) => {
+          if (item.name && (item.name === 'user_name' || item.name === 'user_message')) {
+            if (item.value.match(/^[^a-zA-Z][^a-zA-Z0-9-_\.]{1,20}$/)) {
+              item.style.border = '2px solid green';
+            } else {
+              item.style.border = '2px solid red';
+              bool = false;
+            }
+          } else if (item.name && item.name === 'user_email') {
+            if (item.value.match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/)) {
+              item.style.border = '2px solid green';
+            } else {
+              item.style.border = '2px solid red';
+              bool = false;
+            }
+          } else if (item.name && item.name === 'user_phone') {
+            if (item.value.match(/^\+?[78]([-()]*\d){10}$/)) {
+              item.style.border = '2px solid green';
+            } else {
+              item.style.border = '2px solid red';
+              bool = false;
+            }
+          }
+        });
+
+        if (bool) {
+          statusMessage.innerHTML = `
+            <div class="sk-double-bounce">
+                <div class="sk-child sk-double-bounce-1"></div>
+                <div class="sk-child sk-double-bounce-2"></div>
+            </div>
+          `;
+          button.disabled = true;
+
+          const formData = new FormData(form);
+          let body = {};
+
+          formData.forEach((val, key) => {
+            body[key] = val;
+          });
+
+          const formClear = () => {
+            elementsForm.forEach((item) => {
+              item.value = '';
+              item.style.border = '';
+              button.disabled = false;
+            });
+          };
+
+          postData(body,
+            () => {
+              statusMessage.textContent = successMessage;
+              setTimeout(() => {
+                animation();
+              }, 1500);
+              formClear();
+            },
+            (error) => {
+              statusMessage.textContent = errorMessage;
+              setTimeout(() => {
+                animation();
+              }, 1500);
+              formClear();
+              console.error(error);
+            });
+        }
+      });
+    };
+
+    form.forEach((item) => {
+      formSubmit(item);
+    });
+
+    const animation = () => {
+      const animMessage = document.querySelector('.status__massage');
+      let anim = animMessage.animate([
+        {opacity: '1'},
+        {opacity: '0'}
+      ], 600);
+
+      anim.addEventListener('finish', function () {
+        animMessage.textContent = '';
+        animMessage.remove();
+      })
+    };
+
+    const postData = (body, outputData, errorData) => {
+      const request = new XMLHttpRequest();
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) return;
+        if (request.status === 200) {
+          outputData();
+        } else {
+          errorData(request.status);
+        }
+      });
+
+      request.open('POST', './server.php');
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.send(JSON.stringify(body));
+    };
+  };
+
   countTimer('2 july 2020');
   toggleMenu();
   togglePopup();
@@ -407,4 +533,5 @@ window.addEventListener('DOMContentLoaded', function () {
   slider();
   team();
   calc(100);
+  sendForm();
 });
